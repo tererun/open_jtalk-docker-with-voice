a832d6d5a4906a591970b4f345f17b89b7947e83
u6k <u6k.apps@gmail.com>
2016-03-24 16:39:29 +0900

    `open-jtalk-api`としてのビルド部分を`Dockerfile-api`に退避して、`open_jtalk`コマンドを使用できる`Dockerfile`を作成しました。標準入力から文字列を受け取り、標準出力に音声データを出力します。

---
+++ entrypoint.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+if [ -z "$OPEN_JTALK_VOICE_FILE" ]; then
+  OPEN_JTALK_VOICE_FILE=/usr/local/lib/hts_voice_nitech_jp_atr503_m001-1.05/nitech_jp_atr503_m001.htsvoice
+fi
+
+if [ -z "$OPEN_JTALK_DIC_DIR" ]; then
+  OPEN_JTALK_DIC_DIR=/usr/local/lib/open_jtalk_dic_utf_8-1.09
+fi
+
+if [ -z "$OPEN_JTALK_OPTIONS" ]; then
+  OPEN_JTALK_OPTIONS="-s 48000 -p 240"
+fi
+
+OUTPUT_FILE=`mktemp`
+
+echo open_jtalk -m $OPEN_JTALK_VOICE_FILE -x $OPEN_JTALK_DIC_DIR -ow $OUTPUT_FILE $OPEN_JTALK_OPTIONS >&2
+open_jtalk -m $OPEN_JTALK_VOICE_FILE -x $OPEN_JTALK_DIC_DIR -ow $OUTPUT_FILE $OPEN_JTALK_OPTIONS
+cat $OUTPUT_FILE
